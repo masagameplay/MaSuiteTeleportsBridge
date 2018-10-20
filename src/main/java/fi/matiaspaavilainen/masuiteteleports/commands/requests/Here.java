64fb7a6ea4476b906e3c35b38258a94c45145f04
Masa <masa@simasasolutions.com>
2018-10-20 14:19:42 +0300

    Added tpahere command

---
+++ src/main/java/fi/matiaspaavilainen/masuiteteleports/commands/requests/Here.java
@@ -0,0 +1,42 @@
+package fi.matiaspaavilainen.masuiteteleports.commands.requests;
+
+import fi.matiaspaavilainen.masuiteteleports.MaSuiteTeleports;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class Here {
+    private MaSuiteTeleports plugin;
+
+    public Here(MaSuiteTeleports p) {
+        plugin = p;
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            return false;
+        }
+        if (args.length != 1) {
+            return false;
+        }
+
+        Player p = (Player) sender;
+        ByteArrayOutputStream b = new ByteArrayOutputStream();
+        DataOutputStream out = new DataOutputStream(b);
+        try {
+            out.writeUTF("MaSuiteTeleports");
+            out.writeUTF("TeleportRequestHere");
+            out.writeUTF(sender.getName());
+            out.writeUTF(args[0]);
+            p.sendPluginMessage(plugin, "BungeeCord", b.toByteArray());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+}

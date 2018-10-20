c9db18eec05582ffe6273b6abc6c416b3b847be2
Masa <masa@simasasolutions.com>
2018-10-20 14:08:40 +0300

    Added spawn command

---
+++ src/main/java/fi/matiaspaavilainen/masuiteteleports/commands/spawns/Spawn.java
@@ -0,0 +1,43 @@
+package fi.matiaspaavilainen.masuiteteleports.commands.spawns;
+
+import fi.matiaspaavilainen.masuiteteleports.MaSuiteTeleports;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class Spawn implements CommandExecutor {
+
+
+    private MaSuiteTeleports plugin;
+
+    public Spawn(MaSuiteTeleports p){
+        plugin = p;
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(!(sender instanceof Player)){
+            return false;
+        }
+        if(args.length != 1){
+            return false;
+        }
+
+        ByteArrayOutputStream b = new ByteArrayOutputStream();
+        DataOutputStream out = new DataOutputStream(b);
+        try {
+            out.writeUTF("MaSuiteTeleports");
+            out.writeUTF("SpawnPlayer");
+            out.writeUTF(sender.getName());
+            sender.getServer().sendPluginMessage(plugin,"BungeeCord", b.toByteArray());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+}

81e02d0031691ca77317e8bb3a5f92972264e3b2
Masa <masa@simasasolutions.com>
2018-10-20 14:12:51 +0300

    Added setspawn command

---
+++ src/main/java/fi/matiaspaavilainen/masuiteteleports/commands/spawns/Set.java
@@ -0,0 +1,47 @@
+package fi.matiaspaavilainen.masuiteteleports.commands.spawns;
+
+import fi.matiaspaavilainen.masuiteteleports.MaSuiteTeleports;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class Set implements CommandExecutor {
+
+
+    private MaSuiteTeleports plugin;
+
+    public Set(MaSuiteTeleports p){
+        plugin = p;
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(!(sender instanceof Player)){
+            return false;
+        }
+        if(args.length != 0){
+            return false;
+        }
+
+        Player p = (Player) sender;
+        ByteArrayOutputStream b = new ByteArrayOutputStream();
+        DataOutputStream out = new DataOutputStream(b);
+        try {
+            out.writeUTF("MaSuiteTeleports");
+            out.writeUTF("SetSpawn");
+            out.writeUTF(sender.getName());
+            Location loc = p.getLocation();
+            out.writeUTF(loc.getWorld().getName() + ":" + loc.getX() + ":" + loc.getY() + ":" + loc.getZ() + ":" + loc.getYaw() + ":" + loc.getPitch());
+            p.sendPluginMessage(plugin,"BungeeCord", b.toByteArray());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+}

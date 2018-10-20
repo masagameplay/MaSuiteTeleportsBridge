30f5c8f80182f017efec77cd3034e7d18c2464f7
Masa <masa@simasasolutions.com>
2018-10-20 14:19:16 +0300

    Added tpa command

---
+++ src/main/java/fi/matiaspaavilainen/masuiteteleports/commands/requests/To.java
@@ -0,0 +1,44 @@
+package fi.matiaspaavilainen.masuiteteleports.commands.requests;
+
+import fi.matiaspaavilainen.masuiteteleports.MaSuiteTeleports;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class To implements CommandExecutor {
+    
+    private MaSuiteTeleports plugin;
+
+    public To(MaSuiteTeleports p) {
+        plugin = p;
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            return false;
+        }
+        if (args.length != 1) {
+            return false;
+        }
+
+        Player p = (Player) sender;
+        ByteArrayOutputStream b = new ByteArrayOutputStream();
+        DataOutputStream out = new DataOutputStream(b);
+        try {
+            out.writeUTF("MaSuiteTeleports");
+            out.writeUTF("TeleportRequestTo");
+            out.writeUTF(sender.getName());
+            out.writeUTF(args[0]);
+            p.sendPluginMessage(plugin, "BungeeCord", b.toByteArray());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+}
